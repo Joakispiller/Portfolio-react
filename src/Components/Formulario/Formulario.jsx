442f2e257168c6555e5ebf3f59773b85626a1d56
joakispiller <joakispiller22@gmail.com>
2023-07-15 21:08:51 -0300

    termine el formulario

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -1,105 +1,106 @@
-// import { Box, Button } from '@chakra-ui/react';
-// import"./Formulario.css"
+import { Box, Button } from '@chakra-ui/react';
+import"./Formulario.css"
 
     
     
 
-// const Formulario= () => {
+const Formulario= () => {
 
  
   
  
 
    
-//     return(
+    return(
 
-//         // todoo 
-//         <form action="https://formsubmit.co/joakispiller22@gmail.com" method="POST"  >
+        // todoo 
+        <form action="https://formsubmit.co/joakispiller22@gmail.com" method="POST"  >
 
-//       <Box    mr={{ "base":"2em",  md: '4em', xl: '15em',}} 
-//       mt={{ "base":"2em",  md: '4em', xl: '2.3em',}}
-//       >
+      <Box    mr={{ "base":"2em",  md: '4em', xl: '15em',}} 
+      mt={{ "base":"2em",  md: '4em', xl: '2.3em',}}
+      >
         
-// {/* nameemai */}
-//         <Box  className='flexNM'>
-// {/* nombre */}
-//             <Box  w={{ "base":"20em",  md: '40em', xl: '41em',}}
-//             ml={{ "base":"2em",  md: '4em', xl: '0em',}}
-//             mb={{ "base":"2em",  md: '2em', xl: '0em',}}>
-//                 <input type="name"
-//                 className='Forms' placeholder='Nombre'minLength="1"
-//                name='Nombre'
-//               />
+{/* nameemai */}
+        <Box  className='flexNM'>
+{/* nombre */}
+            <Box  w={{ "base":"20em",  md: '40em', xl: '41em',}}
+            ml={{ "base":"2em",  md: '4em', xl: '0em',}}
+            mb={{ "base":"2em",  md: '2em', xl: '0em',}}>
+                <input type="name"
+                className='Forms' placeholder='Nombre'minLength="1"
+               name='Nombre'
+              />
 
-//            </Box>
+           </Box>
 
-//            {/* email */}
+           {/* email */}
 
-//            <Box  ml={{ "base":"2em",  md: '4em', xl: '1.1em',}}
-//            w={{ "base":"20em",  md: '40em', xl: '40.6em',}}
-//            >
-//            <input type="email"
-//              className='Forms' placeholder='Email' 
-//               name='Email' 
-//         />
-//            </Box>
+           <Box  ml={{ "base":"2em",  md: '4em', xl: '1.1em',}}
+           w={{ "base":"20em",  md: '40em', xl: '40.6em',}}
+           >
+           <input type="email"
+             className='Forms' placeholder='Email' 
+              name='Email' 
+        />
+           </Box>
 
-//         </Box>
-//         {/* tema */}
-//         <Box    mt={{ "base":"2em",  md: '2em', xl: '1.3em',}}
-//          ml={{ "base":"2em",  md: '4em', xl: '0em',}}>
-//         <input type="text" name='Asunto' 
-//         className='Forms' placeholder="Asunto" 
+        </Box>
+        {/* tema */}
+        <Box    mt={{ "base":"2em",  md: '2em', xl: '1.3em',}}
+         ml={{ "base":"2em",  md: '4em', xl: '0em',}}>
+        <input type="text" name='Asunto' 
+        className='Forms' placeholder="Asunto" 
         
-//         />
-//         </Box>
+        />
+        </Box>
 
-//         {/* mensjae */}
-//         <Box mt={{ "base":"2em",  md: '2em', xl: '1.3em',}}
-//          ml={{ "base":"2em",  md: '4em', xl: '0em',}}>
-//           <textarea className='msj' name='Comentarios'
-//           cols="30" rows="10" placeholder='Mensaje' 
-
-
-//          >
-
-//           </textarea>
+        {/* mensjae */}
+        <Box mt={{ "base":"2em",  md: '2em', xl: '1.3em',}}
+         ml={{ "base":"2em",  md: '4em', xl: '0em',}}>
+          <textarea className='msj' name='Comentarios'
+          cols="30" rows="10" placeholder='Mensaje' 
 
 
-//         </Box>
+         >
+
+          </textarea>
+
+
+        </Box>
        
    
-//       </Box>
-//          <Box >
-//          <Button colorScheme='teal' variant='outline' id='btn' type='submit'
-//            color={"#00C296"} fontWeight={350} 
-//            w={{ "base":"11em",  md: '14em', xl: '11em',}}
-//            h={{ "base":"2.5em",  md: '3.5em', xl: '3em',}}
-//            mt={{ "base":"2em",  md: '2.5em', xl: '2.1em',}}
-//            mb={{ "base":"6em",  md: '6em', xl: '0em',}}
-//            fontSize={{ "base":"0.9em",  md: '1.1em', xl: '1em',}}
-//            _hover={{
-//             bg: '#00C296', color:"Black",  
-//           }}
-//            >
-//               Enviar mensaje
-//            </Button>
+      </Box>
+         <Box >
+         <Button colorScheme='teal' variant='outline' id='btn' type='submit'
+           color={"#00C296"} fontWeight={350} 
+           w={{ "base":"11em",  md: '14em', xl: '11em',}}
+           h={{ "base":"2.5em",  md: '3.5em', xl: '3em',}}
+           mt={{ "base":"2em",  md: '2.5em', xl: '2.1em',}}
+           mb={{ "base":"6em",  md: '6em', xl: '0em',}}
+           fontSize={{ "base":"0.9em",  md: '1.1em', xl: '1em',}}
+           _hover={{
+            bg: '#00C296', color:"Black",  
+          }}
+           >
+              Enviar mensaje
+           </Button>
              
           
        
-//         </Box>
-//         <input type="hidden" name="_next" value="http://localhost:5173/Portfolio-react/"></input>
-//         <input type="hidden" name="_captcha" value="false"></input>
+        </Box>
+        <input type="hidden" name="_next" value="https://joakispiller.github.io/Portfolio-react/"></input>
+        <input type="hidden" name="_captcha" value="false"></input>
 
    
         
-//       </form>
+      </form>
       
-//     )
+    )
 
-// }
+}
 
-/* export { Formulario} */
+export { Formulario}
+
 
 
 
@@ -245,124 +246,3 @@
 // }
 
 // export { Formulario} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { Box, Button } from '@chakra-ui/react';
-import"./Formulario.css"
-
-    
-    
-
-const Formulario= () => {
-
- 
-  
- 
-
-   
-    return(
-
-        // todoo 
-        <form name="contact" method="POST" data-netlify="true"  >
-
-      <Box    mr={{ "base":"2em",  md: '4em', xl: '15em',}} 
-      mt={{ "base":"2em",  md: '4em', xl: '2.3em',}}
-      >
-        
-{/* nameemai */}
-        <Box  className='flexNM'>
-{/* nombre */}
-            <Box  w={{ "base":"20em",  md: '40em', xl: '41em',}}
-            ml={{ "base":"2em",  md: '4em', xl: '0em',}}
-            mb={{ "base":"2em",  md: '2em', xl: '0em',}}>
-                <input type="name"
-                className='Forms' placeholder='Nombre'minLength="1"
-               name='Nombre'
-              />
-
-           </Box>
-
-           {/* email */}
-
-           <Box  ml={{ "base":"2em",  md: '4em', xl: '1.1em',}}
-           w={{ "base":"20em",  md: '40em', xl: '40.6em',}}
-           >
-           <input type="email"
-             className='Forms' placeholder='Email' 
-              name='Email' 
-        />
-           </Box>
-
-        </Box>
-        {/* tema */}
-        <Box    mt={{ "base":"2em",  md: '2em', xl: '1.3em',}}
-         ml={{ "base":"2em",  md: '4em', xl: '0em',}}>
-        <input type="text" name='Asunto' 
-        className='Forms' placeholder="Asunto" 
-        
-        />
-        </Box>
-
-        {/* mensjae */}
-        <Box mt={{ "base":"2em",  md: '2em', xl: '1.3em',}}
-         ml={{ "base":"2em",  md: '4em', xl: '0em',}}>
-          <textarea className='msj' name='Comentarios'
-          cols="30" rows="10" placeholder='Mensaje' 
-
-
-         >
-
-          </textarea>
-
-
-        </Box>
-       
-   
-      </Box>
-         <Box >
-         <Button colorScheme='teal' variant='outline' id='btn' type='submit'
-           color={"#00C296"} fontWeight={350} 
-           w={{ "base":"11em",  md: '14em', xl: '11em',}}
-           h={{ "base":"2.5em",  md: '3.5em', xl: '3em',}}
-           mt={{ "base":"2em",  md: '2.5em', xl: '2.1em',}}
-           mb={{ "base":"6em",  md: '6em', xl: '0em',}}
-           fontSize={{ "base":"0.9em",  md: '1.1em', xl: '1em',}}
-           _hover={{
-            bg: '#00C296', color:"Black",  
-          }}
-           >
-              Enviar mensaje
-           </Button>
-             
-          
-       
-        </Box>
-        
-
-   
-        
-      </form>
-      
-    )
-
-}
-export { Formulario} 
